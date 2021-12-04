ea53e3779914e8ac65a85ef8ffd5203a8e62fb6e
Teagan glenn <teagan.m.glenn@gmail.com>
2021-12-04 01:18:08 -0700

    Update platform name

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import 'reflect-metadata';
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+import { UltimateGovePlatform } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, UltimateGovePlatform);
 };
