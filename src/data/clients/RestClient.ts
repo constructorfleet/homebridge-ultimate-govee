7fa309fa70b6bede216789dff6ab3d7a09b12766
Teagan glenn <teagan.m.glenn@gmail.com>
2021-12-03 23:55:32 -0700

    Update client

--- src/data/clients/RestClient.ts
+++ src/data/clients/RestClient.ts
@@ -1,14 +1,19 @@
-import {request} from './request';
+import {request} from '../request';
 import {BaseHeaders} from '../structures/api/requests/headers/baseHeaders';
 import {LoginRequest} from '../structures/api/requests/payloads/loginRequest';
 import {LoginResponse} from '../structures/api/requests/payloads/loginResponse';
 import {AuthenticatedHeader} from '../structures/api/requests/headers/authenticatedHeader';
+import {BaseRequest} from '../structures/api/requests/payloads/base/baseRequest';
+import {AppDeviceListResponse} from '../structures/api/requests/payloads/appDeviceListResponse';
+import {GoveeClient} from './GoveeClient';
+import {ApiHeader} from '../structures/api/requests/headers/ApiHeader';
+import {ApiDeviceListResponse} from '../structures/api/requests/payloads/apiDeviceListResponse';
 
 const BASE_GOVEE_APP_ACCOUNT_URL = 'https://app.govee.com/account/rest/account/v1';
 const BASE_GOVEE_APP_DEVICE_URL = 'https://app2.govee.com/device/rest/devices/v1';
 const GOVEE_API_BASE_URL = 'https://developer-api.govee.com/v1/devices';
 
-export class GoveeClient {
+export class RestClient extends GoveeClient {
   private token?: string;
   private topic?: string;
 
@@ -18,6 +23,7 @@ export class GoveeClient {
     private clientId: string,
     private apiKey?: string,
   ) {
+    super();
   }
 
   async login() {
@@ -40,6 +46,31 @@ export class GoveeClient {
       throw Error(`Unable to authenticate with Govee server. ${err}`);
     });
   }
+
+  async getAppDevices() {
+    await request<AuthenticatedHeader, BaseRequest, AppDeviceListResponse>(
+      `${BASE_GOVEE_APP_DEVICE_URL}/list`,
+      AuthenticatedHeader.build(this.token!),
+      BaseRequest.build(),
+    ).get().then(response => {
+
+    }).catch(err => {
+      throw Error(`Unable to retrieve Govee device list. ${err}`);
+    });
+  }
+
+  async getApiDevices() {
+    await request<ApiHeader, BaseRequest, ApiDeviceListResponse>(
+      `${GOVEE_API_BASE_URL}`,
+      ApiHeader.build(this.apiKey!),
+      BaseRequest.build(),
+    ).get().then(response => {
+
+    }).catch(err => {
+      throw Error(`Unable to retrieve Govee api device list. ${err}`);
+    });
+  }
+
   //
   // curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNpZCI6IjRsNnZFY3I5cVNXamlYaVBNRUVtSGRwT1pCdXRkcmxwIn0sImlhdCI6MTYzNzkwODE3OCwiZXhwIjoxNjQzMDkyMTc4fQ.jCfgJIodT0foFbFWr_Owzn6l84nzqLM9DGvRMkwNNyY" -H "AppVersion: 3.7.0" -H "Content-Type: application/json" -d '{"device": "25:DA:9C:04:A0:49:CF:55", "sku": "H7141", "waterShortageOnOff": 0}' -XPOST https://app2.govee.com/account/rest/account/v1/iot
   //
