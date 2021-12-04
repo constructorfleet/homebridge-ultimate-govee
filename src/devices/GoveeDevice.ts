727e0146367c94121dca6101a751f0b0c449ec1d
Teagan glenn <teagan.m.glenn@gmail.com>
2021-12-04 01:54:44 -0700

    Govee device

--- src/devices/GoveeDevice.ts
+++ src/devices/GoveeDevice.ts
@@ -1,8 +1,14 @@
-export abstract class GoveeDevice {
+import {EventEmitter} from 'events';
+import {API, PlatformAccessory} from 'homebridge';
+
+export abstract class GoveeDevice extends EventEmitter {
   protected constructor(
+    public api: API,
+    public accessory: PlatformAccessory,
     public name: string,
     public deviceId: string,
     public model: string,
   ) {
+    super();
   }
 }
