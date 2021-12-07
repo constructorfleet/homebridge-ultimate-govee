76aa147576796e456884982c29a732de3ae2193a
Teagan glenn <teagan.m.glenn@gmail.com>
2021-12-06 20:38:32 -0700

    iot topic

--- src/util/const.ts
+++ src/util/const.ts
@@ -5,3 +5,5 @@ export const GOVEE_USERNAME = 'Govee_Username';
 export const GOVEE_PASSWORD = 'Govee_Password';
 export const GOVEE_CLIENT_ID = 'Goveee_Client_Id';
 export const GOVEE_API_KEY = 'Govee_Api_Key';
+
+export const IOT_ACCOUNT_TOPIC = 'IOT_Account_Topic';
