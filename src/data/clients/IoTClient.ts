12721a6fcd23e7ecc01327e47f0784b87e533eab
Teagan glenn <teagan.m.glenn@gmail.com>
2021-12-06 20:39:08 -0700

    Events to IoT client

--- src/data/clients/IoTClient.ts
+++ src/data/clients/IoTClient.ts
@@ -1,8 +1,19 @@
 import {device} from 'aws-iot-device-sdk';
 import path from 'path';
 import {GoveeClient} from './GoveeClient';
+import {Emits, Handles} from '../../util/events';
+import {IotMessage} from '../structures/iot/device/IotMessage';
+import {instanceToPlain, plainToInstance} from 'class-transformer';
+import {container} from 'tsyringe';
+import {IOT_ACCOUNT_TOPIC} from '../../util/const';
+import {IoTAccountMessage} from '../structures/iot/account/IoTAccountMessage';
 
-export class AWSClient extends GoveeClient {
+@Emits<IoTClient>(
+  'IoTConnected',
+  'IoTDisconnected',
+  'IoTMessageReceived',
+)
+export class IoTClient extends GoveeClient {
   private awsIOTDevice: device;
 
   constructor(
@@ -20,5 +31,38 @@ export class AWSClient extends GoveeClient {
       caPath: path.resolve(caPath),
       host: host,
     });
+
+    this.awsIOTDevice.on(
+      'connect',
+      () => this.emit('IoTConnected'),
+    );
+
+    this.awsIOTDevice.on(
+      'close',
+      () => this.emit('IoTDisconnected'),
+    );
+
+    this.awsIOTDevice.on(
+      'message',
+      (topic, payload) => this.emit(
+        'IoTMessageReceived',
+        topic === container.resolve<string>(IOT_ACCOUNT_TOPIC)
+          ? plainToInstance(IoTAccountMessage, JSON.parse(payload.toString()))
+          : plainToInstance(IotMessage, JSON.parse(payload.toString())),
+      ),
+    );
+  }
+
+  @Handles('SubscribeToTopic')
+  subscribeTo(topic: string) {
+    this.awsIOTDevice.subscribe(topic);
+  }
+
+  @Handles('PublishToTopic')
+  publishTo(message: IotMessage) {
+    this.awsIOTDevice.publish(
+      message.topic,
+      JSON.stringify(instanceToPlain(message)),
+    );
   }
 }
