97c301905f37b8ca15179a09860368f3c01712ca
Teagan glenn <teagan.m.glenn@gmail.com>
2022-02-10 16:57:52 -0700

    Scene state

---
+++ src/devices/states/Scenes.ts
@@ -0,0 +1,49 @@
+import {State} from './State';
+import {DeviceState} from '../../core/structures/devices/DeviceState';
+import {getCommandCodes, getCommandValues} from '../../util/opCodeUtils';
+import {COMMAND_IDENTIFIER, REPORT_IDENTIFIER} from '../../util/const';
+
+const commandIdentifiers = [
+  5,
+  4,
+];
+
+export interface SceneState {
+  sceneId?: number;
+
+  get sceneChange(): number[];
+}
+
+export function Scene<StateType extends State>(
+  stateType: new (...args) => StateType,
+) {
+  // @ts-ignore
+  return class extends stateType implements SceneState {
+    public sceneId?: number;
+
+    public constructor(...args) {
+      super(...args);
+      this.addDeviceStatusCodes(commandIdentifiers);
+    }
+
+    public override parse(deviceState: DeviceState): ThisType<this> {
+      const commandValues = getCommandValues(
+        [REPORT_IDENTIFIER, ...commandIdentifiers],
+        deviceState.commands,
+      );
+      if (commandValues) {
+        this.sceneId = commandValues[0];
+      }
+
+      return super.parse(deviceState);
+    }
+
+    public get sceneChange(): number[] {
+      return getCommandCodes(
+        COMMAND_IDENTIFIER,
+        commandIdentifiers,
+        this.sceneId || 0,
+      );
+    }
+  };
+}
