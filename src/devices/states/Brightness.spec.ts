10da2e7f42bdf2e12a810fb8de4e0e71b5b40372
Teagan glenn <teagan.m.glenn@gmail.com>
2022-02-09 23:08:04 -0700

    Brightness state

---
+++ src/devices/states/Brightness.spec.ts
@@ -0,0 +1,70 @@
+import {State} from './State';
+import {COMMAND_IDENTIFIER, REPORT_IDENTIFIER} from '../../util/const';
+import {Brightness, BrightnessState} from './Brightness';
+
+class TestState extends Brightness(State) {
+  constructor(...args) {
+    super(...args);
+  }
+}
+
+let testState: BrightnessState & State;
+
+describe('BrightnessState', () => {
+  beforeEach(() => {
+    testState = new TestState();
+  });
+
+  describe('constructor', () => {
+    it('adds command identifier', () => {
+      expect(testState.deviceStatusCodes).toHaveLength(1);
+      expect(testState.deviceStatusCodes).toStrictEqual([[4]]);
+    });
+  });
+
+  describe('parse', () => {
+    it('processes DeviceState.commands', () => {
+      expect(testState.brightness).toBeUndefined();
+      testState.parse({
+        deviceId: 'device',
+        commands: [
+          [REPORT_IDENTIFIER, 4, 10],
+        ],
+      });
+      expect(testState.brightness).toBe(10);
+      testState.parse({
+        deviceId: 'device',
+        commands: [
+          [REPORT_IDENTIFIER, 5, 4, 100],
+        ],
+      });
+      expect(testState.brightness).toBe(100);
+    });
+
+    it('ignores non-applicable DeviceState', () => {
+      expect(testState.brightness).toBeUndefined();
+      testState.parse({
+        deviceId: 'device',
+        brightness: 100,
+        commands: [
+          [REPORT_IDENTIFIER, 1, 2],
+        ],
+      });
+      expect(testState.brightness).toBeUndefined();
+    });
+  });
+
+  describe('brightnessChange', () => {
+    it('returns opcode array', () => {
+      testState.brightness = 22;
+      expect(testState.brightness).toStrictEqual(
+        [
+          COMMAND_IDENTIFIER, 4, 25, 0, 0,
+          0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0,
+          0, 0, 0, 0, 33,
+        ],
+      );
+    });
+  });
+});
---
+++ src/devices/states/Brightness.ts
@@ -0,0 +1,48 @@
+import {State} from './State';
+import {DeviceState} from '../../core/structures/devices/DeviceState';
+import {getCommandCodes, getCommandValues} from '../../util/opCodeUtils';
+import {COMMAND_IDENTIFIER, REPORT_IDENTIFIER} from '../../util/const';
+
+const commandIdentifiers = [
+  4,
+];
+
+export interface BrightnessState {
+  brightness?: number;
+
+  get brightnessChange(): number[];
+}
+
+export function Brightness<StateType extends State>(
+  stateType: new (...args) => StateType,
+) {
+  // @ts-ignore
+  return class extends stateType implements BrightnessState {
+    public brightness?: number;
+
+    public constructor(...args) {
+      super(...args);
+      this.addDeviceStatusCodes(commandIdentifiers);
+    }
+
+    public override parse(deviceState: DeviceState): ThisType<this> {
+      const commandValues = getCommandValues(
+        [REPORT_IDENTIFIER, ...commandIdentifiers],
+        deviceState.commands,
+      );
+      if (commandValues) {
+        this.brightness = commandValues[0];
+      }
+
+      return super.parse(deviceState);
+    }
+
+    public get brightnessChange(): number[] {
+      return getCommandCodes(
+        COMMAND_IDENTIFIER,
+        commandIdentifiers,
+        this.brightness || 0,
+      );
+    }
+  };
+}
