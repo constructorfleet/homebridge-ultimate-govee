7432b6217b20b4cf620df7fe90394da36a85c023
Teagan glenn <teagan.m.glenn@gmail.com>
2022-02-19 03:20:07 -0700

    More transitions

---
+++ src/devices/states/modes/SolidColor.ts
@@ -0,0 +1,39 @@
+import {DeviceMode} from './DeviceMode';
+import {DeviceState} from '../../../core/structures/devices/DeviceState';
+import {getCommandCodes, getCommandValues} from '../../../util/opCodeUtils';
+import {COMMAND_IDENTIFIER, REPORT_IDENTIFIER} from '../../../util/const';
+import {ColorRGB} from '../../../util/colorUtils';
+
+export class SolidColorMode extends DeviceMode {
+  public modeIdentifier = 13;
+  public solidColor: ColorRGB = new ColorRGB(0, 0, 0);
+
+  public parse(deviceState: DeviceState): ThisType<this> {
+    const commandValues = getCommandValues(
+      [
+        REPORT_IDENTIFIER,
+        ...this.commandIdentifiers,
+      ],
+      deviceState.commands,
+    );
+
+    if (commandValues?.length === 1) {
+      const values = commandValues[0];
+      this.solidColor.red = values[0];
+      this.solidColor.green = values[1];
+      this.solidColor.blue = values[2];
+    }
+
+    return this;
+  }
+
+  public colorChange(): number[] {
+    return getCommandCodes(
+      COMMAND_IDENTIFIER,
+      this.commandIdentifiers,
+      this.solidColor.red,
+      this.solidColor.green,
+      this.solidColor.blue,
+    );
+  }
+}
