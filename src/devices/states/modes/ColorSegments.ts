f53bba98390d0af7cffddf0392609e7e5a0596f0
Teagan glenn <teagan.m.glenn@gmail.com>
2022-02-18 20:26:56 -0700

    Color segments mode

---
+++ src/devices/states/modes/ColorSegments.ts
@@ -0,0 +1,107 @@
+import {DeviceState} from '../../../core/structures/devices/DeviceState';
+import {getCommandCodes, getCommandValues} from '../../../util/opCodeUtils';
+import {COMMAND_IDENTIFIER, REPORT_IDENTIFIER, SEGMENT_COUNT} from '../../../util/const';
+import {ColorRGB} from '../../../util/colorUtils';
+import {DeviceMode} from './DeviceMode';
+
+const reportIdentifiers = [
+  165,
+];
+
+export class ColorSegment {
+  constructor(
+    public color: ColorRGB,
+    public brightness: number,
+  ) {
+  }
+}
+
+export class ColorSegmentsMode extends DeviceMode {
+  public modeIdentifier = 21;
+  public colorSegments: ColorSegment[] =
+    Array.from(
+      new Array(SEGMENT_COUNT),
+      () => {
+        return new ColorSegment(
+          new ColorRGB(0, 0, 0),
+          0,
+        );
+      },
+    );
+
+  parse(deviceState: DeviceState): ThisType<this> {
+    const commandValues = getCommandValues(
+      [REPORT_IDENTIFIER, ...reportIdentifiers],
+      deviceState.commands,
+    );
+
+    if ((commandValues?.length || 0) > 0) {
+      commandValues?.forEach(
+        (cmdValues) => {
+          const startIndex = (cmdValues[0] - 1) * 3;
+          for (let i = 0; i < 3; i++) {
+            const brightness = cmdValues[i * 4 + 1];
+            const color = new ColorRGB(
+              cmdValues[i * 4 + 2],
+              cmdValues[i * 4 + 3],
+              cmdValues[i * 4 + 4],
+            );
+            this.colorSegments[startIndex + i] =
+              new ColorSegment(
+                color,
+                brightness,
+              );
+          }
+        },
+      );
+    }
+
+    return this;
+  }
+
+  public colorSegmentsChange(
+    color: ColorRGB,
+    index: number,
+  ): number[] {
+    const leftSegments =
+      (index) < 8
+        ? Math.pow(2, index)
+        : 0;
+    const rightSegments =
+      (index) < 8
+        ? 0
+        : Math.pow(2, index - 8);
+    return getCommandCodes(
+      COMMAND_IDENTIFIER,
+      [5, 21, 1],
+      color.red,
+      color.green,
+      color.blue,
+      0,
+      0,
+      leftSegments,
+      rightSegments,
+    );
+  }
+
+  public brightnessSegmentsChange(
+    brightness: number,
+    index: number,
+  ): number[] {
+    const leftSegments =
+      (index) < 8
+        ? Math.pow(2, index)
+        : 0;
+    const rightSegments =
+      (index) < 8
+        ? 0
+        : Math.pow(2, index - 8);
+    return getCommandCodes(
+      COMMAND_IDENTIFIER,
+      [5, 21, 2],
+      brightness,
+      leftSegments,
+      rightSegments,
+    );
+  }
+}
