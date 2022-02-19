b1ee8bf2800fae4247268e0bb7a781ee03b0c466
Teagan glenn <teagan.m.glenn@gmail.com>
2022-02-18 21:51:49 -0700

    Mode change for segment mode

--- src/core/structures/devices/transitions/DeviceColorSegmentTransition.ts
+++ src/core/structures/devices/transitions/DeviceColorSegmentTransition.ts
@@ -1,10 +1,11 @@
 import {DeviceTransition} from '../DeviceTransition';
 import {GoveeDevice} from '../../../../devices/GoveeDevice';
 import {ColorRGB} from '../../../../util/colorUtils';
-import {ColorSegmentsState} from '../../../../devices/states/ColorSegments';
-import {arrayReplace} from '../../../../util/arrayUtils';
+import {ModesState} from '../../../../devices/states/Modes';
+import {DeviceMode} from '../../../../devices/states/modes/DeviceMode';
+import {ColorSegmentsMode} from '../../../../devices/states/modes/ColorSegments';
 
-export class DeviceColorSegmentTransition extends DeviceTransition<ColorSegmentsState & GoveeDevice> {
+export class DeviceColorSegmentTransition extends DeviceTransition<ModesState & GoveeDevice> {
 
   constructor(
     deviceId: string,
@@ -14,13 +15,54 @@ export class DeviceColorSegmentTransition extends DeviceTransition<ColorSegments
     super(deviceId);
   }
 
-  protected updateState(device: ColorSegmentsState & GoveeDevice): DeviceColorSegmentTransition {
-    arrayReplace(
-      device.colorSegments,
-      this.segmentIndex,
-      this.color,
-    );
-    this.commandCodes = device.colorSegmentsChange;
+  protected updateState(device: ModesState & GoveeDevice): DeviceColorSegmentTransition {
+    const colorSegmentMode = Array.from(
+      device.modes.values(),
+    ).find(
+      (deviceMode: DeviceMode) => deviceMode instanceof ColorSegmentsMode,
+    ) as ColorSegmentsMode;
+    if (!colorSegmentMode) {
+      return this;
+    }
+
+    device.activeMode = colorSegmentMode.modeIdentifier;
+    colorSegmentMode.colorSegments[this.segmentIndex].color.update(this.color);
+
+    this.commandCodes = [
+      device.modeChange,
+      colorSegmentMode.colorSegmentsChange(this.color, this.segmentIndex),
+    ];
+    return this;
+  }
+}
+
+export class DeviceBrightnessSegmentTransition extends DeviceTransition<ModesState & GoveeDevice> {
+
+  constructor(
+    deviceId: string,
+    public readonly segmentIndex: number,
+    public readonly brightness: number,
+  ) {
+    super(deviceId);
+  }
+
+  protected updateState(device: ModesState & GoveeDevice): DeviceBrightnessSegmentTransition {
+    const colorSegmentMode = Array.from(
+      device.modes.values(),
+    ).find(
+      (deviceMode: DeviceMode) => deviceMode instanceof ColorSegmentsMode,
+    ) as ColorSegmentsMode;
+    if (!colorSegmentMode) {
+      return this;
+    }
+
+    device.activeMode = colorSegmentMode.modeIdentifier;
+    colorSegmentMode.colorSegments[this.segmentIndex].brightness = this.brightness;
+
+    this.commandCodes = [
+      device.modeChange,
+      colorSegmentMode.brightnessSegmentsChange(this.brightness, this.segmentIndex),
+    ];
     return this;
   }
 }
