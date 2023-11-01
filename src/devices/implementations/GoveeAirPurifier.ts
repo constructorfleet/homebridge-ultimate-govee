e4175a8853c5f4a0c5cf53a06c50d0c6730db8fa
Teagan glenn <that@teagantotally.rocks>
2023-11-01 09:16:57 +0000

    Move H7126

--- src/devices/implementations/GoveeAirPurifier.ts
+++ src/devices/implementations/GoveeAirPurifier.ts
@@ -1,15 +1,15 @@
-import {OnOff} from '../states/OnOff';
-import {FanSpeed} from '../states/FanSpeed';
-import {Active} from '../states/Active';
-import {Timer} from '../states/Timer';
-import {GoveeDevice} from '../GoveeDevice';
-import {ControlLock} from '../states/ControlLock';
-import {DeviceFactory} from '../DeviceFactory';
+import { OnOff } from '../states/OnOff';
+import { FanSpeed } from '../states/FanSpeed';
+import { Active } from '../states/Active';
+import { Timer } from '../states/Timer';
+import { GoveeDevice } from '../GoveeDevice';
+import { ControlLock } from '../states/ControlLock';
+import { DeviceFactory } from '../DeviceFactory';
+import { ProgrammableFanSpeed } from '../states/ProgrammableFanSpeed';
 
 @DeviceFactory.register(
   'H7121',
   'H7122',
-  'H7126'
 )
 export class GoveeAirPurifier
   extends ControlLock(
@@ -24,6 +24,18 @@ export class GoveeAirPurifier
     ),
   ) {
 
+  constructor(args) {
+    super(args);
+  }
+}
+
+@DeviceFactory.register(
+  'H7126',
+)
+export class GoveeAirPurifierLite
+  extends ProgrammableFanSpeed(
+    GoveeAirPurifier
+  ) {
   constructor(args) {
     super(args);
   }
