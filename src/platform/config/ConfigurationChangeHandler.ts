b658400a3ed90ac364ce79c70e6259abf3bed94d
Teagan glenn <that@teagantotally.rocks>
2023-11-04 14:09:25 +0000

    Trigger device refresh on config change

--- src/platform/config/ConfigurationChangeHandler.ts
+++ src/platform/config/ConfigurationChangeHandler.ts
@@ -5,6 +5,7 @@ import {PlatformConfigBeforeAfter} from './events/PluginConfiguration';
 import deepDiff from 'deep-diff-pizza';
 import jmesPath from 'jmespath';
 import {LoggingService} from '../../logging/LoggingService';
+import { DeviceRefreshEvent } from '../../core/events/devices/DeviceRefresh';
 
 @Injectable()
 export class ConfigurationChangeHandler extends Emitter {
@@ -17,7 +18,10 @@ export class ConfigurationChangeHandler extends Emitter {
   }
 
   @OnEvent(
-    'PLATFORM.CONFIG.Reloaded',
+    'PLATFORM.CONFIG.Reloaded', {
+      async: true,
+      nextTick: true,
+    }
   )
   async onPlatformConfigurationReloaded(
     {
@@ -36,6 +40,31 @@ export class ConfigurationChangeHandler extends Emitter {
       ' ' +
       '}',
     );
+
+    specificChanges.devices.reduce(
+      (acc: Set<string>, cur) => {
+        const deviceTypeIndex: string = cur['path']?.split('.')[1] ?? undefined;
+        if (!deviceTypeIndex) {
+          return acc;
+        }
+        const indexMatch = /.+\[(?<index>\d+)\]/.exec(deviceTypeIndex);
+        if (!indexMatch || !indexMatch.groups) {
+          return acc;
+        }
+        const deviceIndex = indexMatch.groups['index'];
+        const deviceType = deviceTypeIndex.substring(0, deviceTypeIndex.indexOf('['));
+        const devices = before.devices ? before.devices[deviceType] : after.devices ? after.devices[deviceType] : undefined;
+        if (!devices) {
+          return acc;
+        }
+        acc.add(devices[deviceIndex].deviceId);
+      },
+      new Set<string>(),
+    ).forEach(async (deviceId) => await this.emitAsync(
+      new DeviceRefreshEvent({
+        deviceId,
+      }),
+    ));
 
     this.log.info(specificChanges);
   }
